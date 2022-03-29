3156768e8311e4ba6cd6227644b2110607ed6db8
j-guerreiro <joel.borghi@gmail.com>
2022-03-29 20:50:33 -0300

    update

---
+++ busca_binaria.c
@@ -0,0 +1,16 @@
+/*
+  Autor: Joel Guerreiro
+
+  Descrição:
+  
+  Algoritmo de busca binária implementada em linguagem C.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  printf("\nBusca Binária\n");
+  return 0;
+}
---
+++ template.c
@@ -0,0 +1,14 @@
+/*
+Autor: Joel Guerreiro
+
+Descrição
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  printf("\nEste é um template =)\n");
+  return 0;
+}
